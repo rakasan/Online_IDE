4ef4dcddf2c3b7977f27b9437c4b8b28bcf5c9a7
rakasan <racasan.vlad@gmail.com>
2021-01-28 22:45:53 +0200

    Added the function for the directory tree

--- Implementation/index.php
+++ Implementation/index.php
@@ -1,93 +1,64 @@
 <html>
 <head>
+<script>
+function showHint(str)
+{
+if (str.length==0)
+  { 
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+    }
+  }
+
+xmlhttp.open("GET","directory_tree.php?path="+str,true);
+xmlhttp.send();
+}
+</script>
+
 </head>
 <body>
 <?php
-
-	
+include "functions.php";
 	$host = "rcsn.ro";
 	$username ="rcsnro69";
 	$password ="@dm1np0w3r";
 	
-	//$conectare = $username.":".$password."@".$host;
-	//echo $conectare;
 	$con = ftp_connect($host) or die("nu a mers");
 	
 	$login = ftp_login($con,$username,$password);
 	ftp_pasv($con,true);
 	if($login)
-		//echo "a mers logarea";
 		echo "merge";
-		else
+	else
 		echo "nu s-a logat";
-	
-	//$fisiere = ftp_get_filelist($con, '/', true);
-	function foldere_recursiv()
-	
-	$foldere = ftp_nlist($con, 'public_html/htaccess');
-	
-	if($con)
-	echo "activ";
-	var_dump($foldere);
-	/*$structure = array();
-$arraypointer = &$structure;
-foreach ($foldere as $rawfile) {
-    if ($rawfile[0] == '/') {
-        $paths = array_slice(explode('/', str_replace(':', '', $rawfile)), 1);
-        $arraypointer = &$structure;
-        foreach ($paths as $path) {
-            foreach ($arraypointer as $i => $file) {
-                if ($file['text'] == $path) {
-                    $arraypointer = &$arraypointer[ $i ]['children'];
-                    break;
-                }
-            }
-        }
-    } elseif(!empty($rawfile)) {
-        $info = preg_split("/[\s]+/", $rawfile, 9);        
-        $arraypointer[] = array(
-            'text'   => $info[8],
-            'isDir'  => $info[0]{0} == 'd',
-            'size'   => byteconvert($info[4]),
-            'chmod'  => chmodnum($info[0]),
-            'date'   => strtotime($info[6] . ' ' . $info[5] . ' ' . $info[7]),
-            'raw'    => $info
-            // the 'children' attribut is automatically added if the folder contains at least one file
-        );
-    }
-}
+	$lista = adu_foldere($con,'/');
+	echo "<ul>";
+	$nr=0;
+	foreach ($lista as $inregistrare ) 
+	{
+		$nr++;
+		echo "<li ";
+		if($inregistrare['isDir'])
+			echo "id =".$nr. " onclick = \"showHint(".$inregistrare['path']. ")\" ";
+		echo ">". $inregistrare['text']."</a>"."</li>";
+	}
+	echo "</ul>";
 
-// in $structure is all the data
-print_r($structure);
-
-// little helper functions
-function byteconvert($bytes) {
-    $symbol = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-    $exp = floor( log($bytes) / log(1024) );
-    return sprintf( '%.2f ' . $symbol[ $exp ], ($bytes / pow(1024, floor($exp))) );
-}
-
-function chmodnum($chmod) {
-    $trans = array('-' => '0', 'r' => '4', 'w' => '2', 'x' => '1');
-    $chmod = substr(strtr($chmod, $trans), 1);
-    $array = str_split($chmod, 3);
-    return array_sum(str_split($array[0])) . array_sum(str_split($array[1])) . array_sum(str_split($array[2]));
-}
-
-	
-	
-	
-	
-	
-	
-	
-	//foreach($f as $fisiere)
-	//echo $f['name'];
-	//echo $foldere;
-	//var_dump($foldere);
-	//foreach(f in $foldere)
-	//echo f;
-	*/
 ?>
+<div id="txtHint"></div>
 </body>
 </html>
